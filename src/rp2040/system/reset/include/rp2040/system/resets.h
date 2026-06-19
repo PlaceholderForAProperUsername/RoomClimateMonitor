0e0191e5dcc4a2f24bfcaa86392d2bcbd7fd87fa
PlaceholderForAProperUsername <thorsten.hoffmeister@protonmail.com>
2026-06-19 10:20:59 +0200

    feat [reset.h]: - add register aliases to the Resets_Type object.

--- src/rp2040/system/reset/include/rp2040/system/resets.h
+++ src/rp2040/system/reset/include/rp2040/system/resets.h
@@ -1,12 +1,52 @@
-//
-//  on 16.06.26.
-//
+/**
+*  @file resets.h
+ * @brief The resets controller to enable or disable subsystems.
+ * @author Thorsten Hoffmeister
+ * @version 0.1
+ * @date 19.06.2026
+ *
+ *
+ * @copyright (C) 2026  Thorsten Hoffmeister
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef RP2040_SYSTEM_RESETS_H
 #define RP2040_SYSTEM_RESETS_H
 
 #include "resets_def.h"
 
+namespace rp2040::system {
+    /**
+    * @addtogroup rp2040_resets
+    * @{
+    */
+
+    template<std::uintptr_t resets_addr>
+    class Resets_Type {
+        static_assert(resets_addr == resets_base, "The resets address must be a valid address");
+
+        using resets_reg = Resets_RegMapType<resets_addr>; /**< @brief The registers of the reset controller */
+        using reset_r = resets_reg::reset_r; /**< @brief The reset register. */
+        using wdsel_r = resets_reg::wdsel_r; /**< @brief The watchdog select regiser. */
+        using reset_done_r = resets_reg::reset_done_r; /**< @brief The reset done register. */
+
+    };
+
+
+    /** @} */ // rp2040_resets
+} // rp2040::system
 
 #include "../../../resets.tpp"
 
