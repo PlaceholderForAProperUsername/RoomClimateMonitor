580f4342876ee30ac2a5ab026767312d3d09289f
PlaceholderForAProperUsername <thorsten.hoffmeister@protonmail.com>
2026-07-28 23:28:40 +0200

    feat [gpio_def]: - add ctrl register to GPIO regmap to set the gpio function and configure overrides.
    refac [gpio_def]: - change status_r to status_reg

--- src/rp2040/system/gpio/include/rp2040/system/gpio_def.h
+++ src/rp2040/system/gpio/include/rp2040/system/gpio_def.h
@@ -127,79 +127,220 @@ namespace rp2040::system::gpio {
          */
         struct status {
             static constexpr std::uintptr_t addr = base_addr + statusOffset; /** @brief The address of the gpio's status register. */
-            using status_r = utils::reg_access::Reg<addr, utils::reg_access::read_access, std::uint32_t>; /** @brief The status register. */
+            using status_reg = utils::reg_access::Reg<addr, utils::reg_access::read_access, std::uint32_t>; /** @brief The status register. */
 
             /**
              * @brief Bitfield for the status bit "interrupt to processors, after override is applied".
              */
-            using IRQToProcBitfield = utils::reg_access::BitFieldEnableDisable<status_r, 26U>;
+            using IRQToProcBitfield = utils::reg_access::BitFieldEnableDisable<status_reg, 26U>;
             /**
              * @brief The status bit "interrupt to processors, after override is applied".
              */
-            using irqToProc_bits = status_r::template Bits<IRQToProcBitfield, utils::reg_access::read_access>;
+            using irqToProc_bits = status_reg::template Bits<IRQToProcBitfield, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "interrupt from pad before override is applied".
              */
-            using IRQFromPadBitField = utils::reg_access::BitFieldEnableDisable<status_r, 24U>;
+            using IRQFromPadBitField = utils::reg_access::BitFieldEnableDisable<status_reg, 24U>;
             /**
              * @brief The status bit "interrupt from pad before override is applied".
              */
-            using irqFromPad_bits = status_r::template Bits<IRQFromPadBitField, utils::reg_access::read_access>;
+            using irqFromPad_bits = status_reg::template Bits<IRQFromPadBitField, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "input signal to peripheral, after override is applied".
              */
-            using IntToPeriBitField = utils::reg_access::BitFieldEnableDisable<status_r, 19U>;
+            using IntToPeriBitField = utils::reg_access::BitFieldEnableDisable<status_reg, 19U>;
             /**
              * @brief The status bit "input signal to peripheral, after override is applied".
              */
-            using intToPeri_bits = status_r::template Bits<IntToPeriBitField, utils::reg_access::read_access>;
+            using intToPeri_bits = status_reg::template Bits<IntToPeriBitField, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "input signal from pad, before override is applied".
              */
-            using IntFromPadBitField = utils::reg_access::BitFieldEnableDisable<status_r, 17U>;
+            using IntFromPadBitField = utils::reg_access::BitFieldEnableDisable<status_reg, 17U>;
             /**
              * @brief The status bit "input signal from pad, before override is applied".
              */
-            using intFromPad_bits = status_r::template Bits<IntFromPadBitField, utils::reg_access::read_access>;
+            using intFromPad_bits = status_reg::template Bits<IntFromPadBitField, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "output enable to pad after register override is applied".
              */
-            using OEToPadBitField = utils::reg_access::BitFieldEnableDisable<status_r, 13U>;
+            using OEToPadBitField = utils::reg_access::BitFieldEnableDisable<status_reg, 13U>;
             /**
              * @brief The status bit "output enable to pad after register override is applied".
              */
-            using oeToPad_bits = status_r::template Bits<OEToPadBitField, utils::reg_access::read_access>;
+            using oeToPad_bits = status_reg::template Bits<OEToPadBitField, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "output enable from selected peripheral, before register override is applied".
              */
-            using OEFromPeriBitField = utils::reg_access::BitFieldEnableDisable<status_r, 12U>;
+            using OEFromPeriBitField = utils::reg_access::BitFieldEnableDisable<status_reg, 12U>;
             /**
              * @brief The status bit "output enable from selected peripheral, before register override is applied".
              */
-            using oeFromPeri_bits = status_r::template Bits<OEFromPeriBitField, utils::reg_access::read_access>;
+            using oeFromPeri_bits = status_reg::template Bits<OEFromPeriBitField, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "output signal to pad after register override is applied".
              */
-            using OutToPadBitField = utils::reg_access::BitFieldEnableDisable<status_r, 9U>;
+            using OutToPadBitField = utils::reg_access::BitFieldEnableDisable<status_reg, 9U>;
             /**
              * @brief The status bit "output signal to pad after register override is applied".
              */
-            using outToPad_bits = status_r::template Bits<OutToPadBitField, utils::reg_access::read_access>;
+            using outToPad_bits = status_reg::template Bits<OutToPadBitField, utils::reg_access::read_access>;
 
             /**
              * @brief Bitfield for the status bit "output signal from selected peripheral, before register override is applied".
              */
-            using OutFromPeriField = utils::reg_access::BitFieldEnableDisable<status_r, 8U>;
+            using OutFromPeriField = utils::reg_access::BitFieldEnableDisable<status_reg, 8U>;
             /**
              * @brief The status bit "output signal from selected peripheral, before register override is applied".
              */
-            using outFromPeri_bits = status_r::template Bits<OutFromPeriField, utils::reg_access::read_access>;
+            using outFromPeri_bits = status_reg::template Bits<OutFromPeriField, utils::reg_access::read_access>;
+        };
+
+        /**
+         * @brief GPIO control register to set the function and overrides.
+         */
+        struct ctrl {
+            static constexpr std::uintptr_t addr = base_addr + ctrlOffset; /** @brief The address of the gpio's ctrl register. */
+            using ctrl_reg = utils::reg_access::Reg<addr, utils::reg_access::read_write_access, std::uint32_t>;
+
+            /**
+             * @brief The interrupt override bit field
+             */
+            struct IRQOverBitField {
+                using reg = ctrl_reg; /**< @brief The register to which the bitfield belongs. */
+                using T = reg::RegType; /**< @brief The type of the register. */
+
+                static constexpr T position = 0x28U; /**< @brief The position of the bits in the register. */
+                static constexpr T mask = (0x03U << position); /**< @brief The mask of the bits. */
+
+                /**
+                 * @brief The override values for the interrupt.
+                 */
+                enum class value : T {
+                    NORMAL = 0x0U, /** @brief Interrupt set to normal. */
+                    INVERT = 0x1U, /** @brief Interrupt is inverted. */
+                    LOW = 0x02U, /** @brief Interrupt is driven low. */
+                    HIGH = 0x3U, /** @brief Interrupt is driven high. */
+                };
+            };
+
+            /**
+             * @brief The interrupt override bits.
+             */
+            using irqOver_bits = ctrl_reg::template Bits<IRQOverBitField>;
+
+            /**
+             * @brief The input override bit field
+             */
+            struct InOverBitField {
+                using reg = ctrl_reg; /**< @brief The register to which the bitfield belongs. */
+                using T = reg::RegType; /**< @brief The type of the register. */
+
+                static constexpr T position = 0x16U; /**< @brief The position of the bits in the register. */
+                static constexpr T mask = (0x03U << position); /**< @brief The mask of the bits. */
+
+                /**
+                 * @brief The override values for the input.
+                 */
+                enum class value : T {
+                    NORMAL = 0x0U, /** @brief Peripheral input set to normal. */
+                    INVERT = 0x1U, /** @brief Peripheral input is inverted. */
+                    LOW = 0x02U, /** @brief Peripheral input is driven low. */
+                    HIGH = 0x3U, /** @brief Peripheral input is driven high. */
+                };
+            };
+
+            /**
+             * @brief The input override bits.
+             */
+            using inOver_bits = ctrl_reg::template Bits<InOverBitField>;
+
+            /**
+             * @brief The output enable override bit field
+             */
+            struct OutEnOverBitField {
+                using reg = ctrl_reg; /**< @brief The register to which the bitfield belongs. */
+                using T = reg::RegType; /**< @brief The type of the register. */
+
+                static constexpr T position = 0x12U; /**< @brief The position of the bits in the register. */
+                static constexpr T mask = (0x03U << position); /**< @brief The mask of the bits. */
+
+                /**
+                 * @brief The override values for output enable.
+                 */
+                enum class value : T {
+                    NORMAL = 0x0U, /** @brief Output enable from peripheral signal selected by funcsel is normal. */
+                    INVERT = 0x1U, /** @brief Output enable from peripheral signal selected by funcsel is inverted. */
+                    DISABLE = 0x02U, /** @brief Disable output. */
+                    ENABLE = 0x3U, /** @brief Enable output. */
+                };
+            };
+
+            /**
+             * @brief The output enable override bits.
+             */
+            using outEnOver_bits = ctrl_reg::template Bits<OutEnOverBitField>;
+
+            /**
+             * @brief The output override bit field
+             */
+            struct OutOverBitField {
+                using reg = ctrl_reg; /**< @brief The register to which the bitfield belongs. */
+                using T = reg::RegType; /**< @brief The type of the register. */
+
+                static constexpr T position = 0x08U; /**< @brief The position of the bits in the register. */
+                static constexpr T mask = (0x03U << position); /**< @brief The mask of the bits. */
+
+                /**
+                 * @brief The override values for output.
+                 */
+                enum class value : T {
+                    NORMAL = 0x0U, /** @brief Output from peripheral signal selected by funcsel is normal. */
+                    INVERT = 0x1U, /** @brief Output from peripheral signal selected by funcsel is inverted. */
+                    LOW = 0x02U, /** @brief Output is driven low. */
+                    HIGH = 0x3U, /** @brief Output is driven high. */
+                };
+            };
+
+            /**
+             * @brief The output override bits.
+             */
+            using outOver_bits = ctrl_reg::template Bits<OutOverBitField>;
+
+            /**
+             * @brief The function select bit field
+             */
+            struct FuncSelectBitField {
+                using reg = ctrl_reg; /**< @brief The register to which the bitfield belongs. */
+                using T = reg::RegType; /**< @brief The type of the register. */
+
+                static constexpr T position = 0x00U; /**< @brief The position of the bits in the register. */
+                static constexpr T mask = (0x1FU << position); /**< @brief The mask of the bits. */
+
+                /**
+                 * @brief The functions the GPIO can be configured for.
+                 *
+                 * GPIO20 - GPIO25 can additionally be configured for clock input/output.
+                 */
+                using value = decltype([]() {
+                    if constexpr ((gpio >= GPIO::GPIO20) && (gpio <= GPIO::GPIO25)) {
+                        return std::type_identity_t<GPIO_FunctionsWithClock>();
+                    } else {
+                        return std::type_identity_t<GPIO_Functions>();
+                    }
+                }());
+            };
+
+            /**
+             * @brief The function select bits.
+             */
+            using funcSelect_bits = ctrl_reg::template Bits<FuncSelectBitField>;
         };
     };
 
